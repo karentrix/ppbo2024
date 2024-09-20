6e76e2743315e0caa79f25183295a4f6e02e087d
karentrix <karentrixie7@gmail.com>
2024-09-20 13:58:37 +0700

    Praktikum 6C - Memisahkan Class di Dalam Folder Masing-masing

---
+++ App/Admin/HomePage.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Admin;
+class HomePage
+{
+    public function tampil() : void {
+        echo "Halaman depan admin";
+    }
+}
+
---
+++ App/Guest/HomePage.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Guest;
+class HomePage
+{
+    public function tampil() : void {
+        echo "Halaman depan guest";
+    }
+}
+
+
+
